b3653769eb263ccb5a23eceaec4e3a8fe6981d33
kiyotaka <dreznuk23@gmail.com>
2023-08-11 15:02:26 +0300

    Str exercise's

---
+++ name_cases.py
@@ -0,0 +1,31 @@
+
+# 2.3
+name = 'Eric'
+message = f'Hello {name}, would you like to learn some Python today?'
+print(message)
+
+# 2.4
+message_lower = f'Hello {name.lower()}, would you like to learn some Python today?'
+message_upper = f'Hello {name.upper()}, would you like to learn some Python today?\n'
+message_title = message.title()
+print(message_title, end='\n')
+print(message_lower, end='\n')
+print(message_upper, end='\n')
+
+# 2.5
+Author = 'Ла Роче Фокалд'
+Quote = 'Требуется огромный талант и умение, чтобы скрыть свой талант и умение.'
+Famous_quote = f'{Author} однажды сказал(а), "{Quote}"'
+print(Famous_quote)
+
+# 2.6 = 2.5
+
+# 2.7
+
+name_user = ' \n\tkiyo '
+print(name_user)
+print(name_user.lstrip())
+print(name_user.rstrip())
+print(name_user.strip())
+
+# Я никогда и не думал смотреть на кого-либо сверху вниз. Потому что успехи или неудачи других не имеют ко мне никакого отношения.
